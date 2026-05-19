eabca9d74fb83f055f8e3703020ac71ca2f28a40
Bubbler <bubbler9903@gmail.com>
2026-05-19 09:53:03 +0900

    Try adding log to troubleshoot collection update fail issue

--- src/routes/api/collections/[id].ts
+++ src/routes/api/collections/[id].ts
@@ -189,6 +189,7 @@ export async function PUT(event: APIEvent) {
         ]
       : []),
   ];
+  console.log(replaceQueries.map(query => query.toSQL()));
 
   if ("batch" in db && typeof db.batch === "function") {
     await db.batch(replaceQueries);
